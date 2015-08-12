1996b950961135db1caf4671e0b7eaf7c7dc4ad7
Antonio Goncalves <antonio.goncalves@gmail.com>
2015-08-12 09:32:49 +0200

    Adding JSF Beans scope

---
+++ 01-roaster/src/main/java/org/agoncal/sample/forge/roaster/BeanScope.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.agoncal.sample.forge.roaster;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+public enum BeanScope
+{
+   DEPENDENT("", false),
+   APPLICATION("javax.enterprise.context.ApplicationScoped", true),
+   SESSION("javax.enterprise.context.SessionScoped", true),
+   CONVERSATION("javax.enterprise.context.ConversationScoped", true),
+   REQUEST("javax.enterprise.context.RequestScoped", false),
+   CUSTOM(null, false);
+
+   private String annotation;
+   private boolean serializable;
+
+   private BeanScope(String annotation, boolean serializable)
+   {
+      this.annotation = annotation;
+      this.serializable = serializable;
+   }
+
+   public String getAnnotation()
+   {
+      return annotation;
+   }
+
+   public boolean isSerializable() {
+      return serializable;
+   }
+}
--- 01-roaster/src/main/java/org/agoncal/sample/forge/roaster/FacesBean.java
+++ 01-roaster/src/main/java/org/agoncal/sample/forge/roaster/FacesBean.java
@@ -1,6 +1,11 @@
 package org.agoncal.sample.forge.roaster;
 
+import java.io.Serializable;
+
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
@@ -14,9 +19,34 @@ public class FacesBean
    public static void main(String[] args)
    {
 
-      final JavaClassSource javaClass = Roaster.create(JavaClassSource.class);
-      javaClass.setPackage("org.agoncal.myproj").setName("MyJSFBean").addAnnotation(Inject.class);
+      final JavaClassSource source = Roaster.create(JavaClassSource.class);
+      source.setPackage("org.agoncal.myproj").setName("MyJSFBean").addAnnotation(Named.class);
 
-      System.out.println(javaClass);
+      BeanScope scopedValue = BeanScope.REQUEST;
+
+      if (BeanScope.DEPENDENT != scopedValue)
+      {
+         source.addAnnotation(scopedValue.getAnnotation());
+         if (scopedValue.isSerializable())
+         {
+            source.addInterface(Serializable.class);
+            source.addField().setPrivate().setStatic(true).setFinal(true).setName("serialVersionUID").setType("long")
+                     .setLiteralInitializer("1L");
+
+            if (BeanScope.CONVERSATION == scopedValue)
+            {
+               // Field
+               source.addField().setName("conversation").setType(Conversation.class).addAnnotation(Inject.class);
+               // Methods
+               source.addMethod().setPublic().setName("begin").setReturnType(String.class)
+                        .setBody("conversation.begin();\n" + "return null;");
+               source.addMethod().setPublic().setName("end").setReturnType(String.class)
+                        .setBody("conversation.end();\n" + "return null;");
+            }
+
+         }
+      }
+
+      System.out.println(source);
    }
 }
